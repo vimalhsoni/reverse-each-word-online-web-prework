dc891fe9e100196236ee011ee5f495c582fb9a8f
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-07 05:00:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  new_sentence = sentence.split(" ").collect do |word|
+  new_sentence = sentence.split(" ").collect do|word|
   word.reverse
 end
   new_sentence.join(" ")
